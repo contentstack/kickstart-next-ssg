d14496511f285aa040891dfc376eea04d7548bd9
Tim Benniks <tbenniks@gmail.com>
2025-04-09 12:21:57 +0200

    feat: initialize Next.js project with Contentstack integration
    - Add package.json with dependencies for Next.js, React, and Contentstack SDKs.
    - Create _app.tsx for custom App component.
    - Implement _document.tsx for custom Document structure.
    - Add global styles with Tailwind CSS in globals.css.
    - Create index.tsx for the main page with Contentstack data fetching.
    - Set up PostCSS configuration for Tailwind CSS.
    - Add TypeScript configuration in tsconfig.json for strict type checking.

---
+++ pages/index.tsx
@@ -0,0 +1,117 @@
+import { getPage, initLivePreview } from "@/lib/contentstack";
+import { Page } from "@/lib/types";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import ContentstackLivePreview from "@contentstack/live-preview-utils";
+
+type PageProps = {
+  staticPropsPage: Page;
+};
+
+export async function getStaticProps() {
+  const staticPropsPage = await getPage("/");
+
+  return {
+    props: { staticPropsPage },
+  };
+}
+
+export default function Index({ staticPropsPage }: PageProps) {
+  const [page, setPage] = useState<Page>(staticPropsPage);
+
+  const getContent = async () => {
+    const page = await getPage("/");
+    setPage(page as Page);
+  };
+
+  useEffect(() => {
+    initLivePreview();
+    ContentstackLivePreview.onEntryChange(getContent);
+  }, []);
+
+  return (
+    <main className="max-w-(--breakpoint-md) mx-auto">
+      <section className="p-4">
+        {page?.title ? (
+          <h1
+            className="text-4xl font-bold mb-4"
+            {...(page?.$ && page?.$.title)}
+          >
+            {page?.title}
+          </h1>
+        ) : null}
+
+        {page?.description ? (
+          <p className="mb-4" {...(page?.$ && page?.$.description)}>
+            {page?.description}
+          </p>
+        ) : null}
+
+        {page?.image ? (
+          <Image
+            className="mb-4"
+            width={768}
+            height={414}
+            src={page?.image.url}
+            alt={page?.image.title}
+            {...(page?.image?.$ && page?.image?.$.url)}
+          />
+        ) : null}
+
+        {page?.rich_text ? (
+          <div
+            {...(page?.$ && page?.$.rich_text)}
+            dangerouslySetInnerHTML={{ __html: page?.rich_text }}
+          />
+        ) : null}
+
+        <div className="space-y-8 max-w-full mt-4">
+          {page?.blocks?.map((item, index) => {
+            const { block } = item;
+            const isImageLeft = block.layout === "image_left";
+
+            return (
+              <div
+                key={block._metadata.uid}
+                {...(page?.$ && page?.$[`blocks__${index}`])}
+                className={`flex flex-col md:flex-row items-center space-y-4 md:space-y-0 bg-white ${
+                  isImageLeft ? "md:flex-row" : "md:flex-row-reverse"
+                }`}
+              >
+                <div className="w-full md:w-1/2">
+                  {block.image ? (
+                    <Image
+                      src={block.image.url}
+                      alt={block.image.title}
+                      width={200}
+                      height={112}
+                      className="w-full"
+                      {...(block?.$ && block?.$.image)}
+                    />
+                  ) : null}
+                </div>
+                <div className="w-full md:w-1/2 p-4">
+                  {block.title ? (
+                    <h2
+                      className="text-2xl font-bold"
+                      {...(block?.$ && block?.$.title)}
+                    >
+                      {block.title}
+                    </h2>
+                  ) : null}
+                  {block.copy ? (
+                    <div
+                      {...(block?.$ && block?.$.copy)}
+                      dangerouslySetInnerHTML={{ __html: block.copy }}
+                      className="prose"
+                    />
+                  ) : null}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </main>
+  );
+}
