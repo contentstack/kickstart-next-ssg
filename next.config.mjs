49b45512819e99f1e9745c05c8b76572898ec6cc
raj pandey <raj.pandey@contentstack.com>
2026-02-09 13:14:28 +0530

    fix: Updated next to v16

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    minimumCacheTTL: 60,
     // Configure allowed hostnames for Next.js Image Optimization
     remotePatterns: [
       ...(process.env.NEXT_PUBLIC_CONTENTSTACK_IMAGE_HOSTNAME
